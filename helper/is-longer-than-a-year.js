2d177da708c6cdeba8777cc1b238600727118cb5
Tobias <tobi.l@posteo.at>
2020-01-17 17:41:18 +0100

    Chore/typo (#4)
    * fix: Typo and improvement
    
    * chore: Add coverage output to test

--- helper/is-longer-than-a-year.js
+++ helper/is-longer-than-a-year.js
@@ -1,7 +1,7 @@
-const isLongerThenAYear = joinedAt => {
+const isLongerThanAYear = joinedAt => {
 	const dateString = new Date(joinedAt);
 	dateString.setFullYear(dateString.getFullYear() + 1);
 	return dateString < Date.now();
 };
 
-export {isLongerThenAYear};
+export {isLongerThanAYear};
--- test/is-longer-than-a-year.test.js
+++ test/is-longer-than-a-year.test.js
@@ -1,15 +1,15 @@
-import {isLongerThenAYear} from '../helper/is-longer-then-a-year';
+import {isLongerThanAYear} from '../helper/is-longer-than-a-year';
 
 test('should return false for today', () => {
 	const dateNow = new Date();
 	const dateString = dateNow.toLocaleString('en-US').split(',')[0];
 
-	const result = isLongerThenAYear(dateString);
+	const result = isLongerThanAYear(dateString);
 	expect(result).toBeFalsy();
 });
 
 test('should return false for date in future', () => {
-	const result = isLongerThenAYear('01/01/2999');
+	const result = isLongerThanAYear('01/01/2999');
 	expect(result).toBeFalsy();
 });
 
@@ -18,7 +18,7 @@ test('should return true for date exact a year ago', () => {
 	dateAYearAgo.setFullYear(dateAYearAgo.getFullYear() - 1);
 	const dateString = dateAYearAgo.toLocaleString('en-US').split(',')[0];
 
-	const result = isLongerThenAYear(dateString);
+	const result = isLongerThanAYear(dateString);
 	expect(result).toBeTruthy();
 });
 
@@ -27,6 +27,6 @@ test('should return true for date more then a year ago', () => {
 	dateAYearAgo.setFullYear(dateAYearAgo.getFullYear() - 2);
 	const dateString = dateAYearAgo.toLocaleString('en-US').split(',')[0];
 
-	const result = isLongerThenAYear(dateString);
+	const result = isLongerThanAYear(dateString);
 	expect(result).toBeTruthy();
 });
